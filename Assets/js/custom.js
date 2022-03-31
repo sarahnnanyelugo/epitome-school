db1d4311092bb8c76c1db4bc31fecf94374cc6c9
Sarah Nnanyelugo <sarahnnanyelugo@gmail.com>
2022-04-01 00:07:16 +0100

    images

--- Assets/js/custom.js
+++ Assets/js/custom.js
@@ -53,29 +53,24 @@ jQuery(document).ready(function () {
 
   window.setTimeout(function () {
     $("#about").removeClass("activenavbar");
-    $("#admission").removeClass("activenavbar");
+    $("#academics").removeClass("activenavbar");
     $("#learning").removeClass("activenavbar");
-    $("#student").removeClass("activenavbar");
-    $("#protection").removeClass("activenavbar");
+    $("#school").removeClass("activenavbar");
     $("#gallery").removeClass("activenavbar");
     $("#news").removeClass("activenavbar");
 
     if ($("#tag").val() == "about") {
       $("#about").addClass("activenavbar");
     }
-    if ($("#tag").val() == "admission") {
-      $("#admission").addClass("activenavbar");
+    if ($("#tag").val() == "academics") {
+      $("#academics").addClass("activenavbar");
     }
     if ($("#tag").val() == "learning") {
       $("#learning").addClass("activenavbar");
     }
 
-    if ($("#tag").val() == "student") {
-      $("#student").addClass("activenavbar");
-    }
-
-    if ($("#tag").val() == "protection") {
-      $("#protection").addClass("activenavbar");
+    if ($("#tag").val() == "school") {
+      $("#school").addClass("activenavbar");
     }
 
     if ($("#tag").val() == "gallery") {
@@ -91,22 +86,17 @@ jQuery(document).ready(function () {
 //for mobile nav
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "80%";
+  document.getElementById("mySidenav").style.width = "70%";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0%";
 }
-//mobilenav
 
-/* When the user clicks on the button,
- toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches(".dropbtn")) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -124,7 +114,6 @@ function myFunction2() {
   document.getElementById("myDropdown222").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches(".dropbtn")) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -137,12 +126,66 @@ window.onclick = function (event) {
     }
   }
 };
+
+function myFunction4() {
+  document.getElementById("myDropdown444").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+function myFunction5() {
+  document.getElementById("myDropdown555").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+
+
+function myFunction6() {
+  document.getElementById("myDropdown666").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+
 
 function myFunction3() {
   document.getElementById("myDropdown333").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches(".dropbtn")) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -155,6 +198,8 @@ window.onclick = function (event) {
     }
   }
 };
+
+//END OF SIDENAV
 
 //for first slider
 
@@ -191,45 +236,40 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
-
-
-
-
-
 //second slider
 
-
-   var slideIndex2 = 1;
+var slideIndex2 = 1;
 showSlides2(slideIndex2);
 
 // Next/previous controls
 function plusSlides2(n) {
-  showSlides2(slideIndex2 += n);
+  showSlides2((slideIndex2 += n));
 }
 
 // Thumbnail image controls
 function currentSlide2(n) {
-  showSlides2(slideIndex2 = n);
+  showSlides2((slideIndex2 = n));
 }
 
 function showSlides2(n) {
   var i;
   var slides2 = document.getElementsByClassName("mySlides2");
   var dots2 = document.getElementsByClassName("dot2");
-  if (n > slides2.length) {slideIndex2 = 1}
-  if (n < 1) {slideIndex2 = slides2.length}
+  if (n > slides2.length) {
+    slideIndex2 = 1;
+  }
+  if (n < 1) {
+    slideIndex2 = slides2.length;
+  }
   for (i = 0; i < slides2.length; i++) {
-      slides2[i].style.display = "none";
+    slides2[i].style.display = "none";
   }
   for (i = 0; i < dots2.length; i++) {
-      dots2[i].className = dots2[i].className.replace(" active2", "");
+    dots2[i].className = dots2[i].className.replace(" active2", "");
   }
-  slides2[slideIndex2-1].style.display = "block";
-  dots2[slideIndex2-1].className += " active2";
+  slides2[slideIndex2 - 1].style.display = "block";
+  dots2[slideIndex2 - 1].className += " active2";
 }
-
-
-
 
 //third slider
 
@@ -238,30 +278,33 @@ showSlides3(slideIndex3);
 
 // Next/previous controls
 function plusSlides3(n) {
-  showSlides3(slideIndex3 += n);
+  showSlides3((slideIndex3 += n));
 }
 
 // Thumbnail image controls
 function currentSlide3(n) {
-  showSlides3(slideIndex3 = n);
+  showSlides3((slideIndex3 = n));
 }
 
 function showSlides3(n) {
   var i;
   var slides3 = document.getElementsByClassName("mySlides3");
   var dots3 = document.getElementsByClassName("dot3");
-  if (n > slides3.length) {slideIndex3 = 1}
-  if (n < 1) {slideIndex3 = slides3.length}
+  if (n > slides3.length) {
+    slideIndex3 = 1;
+  }
+  if (n < 1) {
+    slideIndex3 = slides3.length;
+  }
   for (i = 0; i < slides3.length; i++) {
-      slides3[i].style.display = "none";
+    slides3[i].style.display = "none";
   }
   for (i = 0; i < dots3.length; i++) {
-      dots3[i].className = dots3[i].className.replace(" active3", "");
+    dots3[i].className = dots3[i].className.replace(" active3", "");
   }
-  slides3[slideIndex3-1].style.display = "block";
-  dots3[slideIndex3-1].className += " active3";
+  slides3[slideIndex3 - 1].style.display = "block";
+  dots3[slideIndex3 - 1].className += " active3";
 }
-
 
 //fourth slider
 
@@ -270,63 +313,60 @@ showSlides4(slideIndex4);
 
 // Next/previous controls
 function plusSlides4(n) {
-  showSlides4(slideIndex4 += n);
+  showSlides4((slideIndex4 += n));
 }
 
 // Thumbnail image controls
 function currentSlide4(n) {
-  showSlides4(slideIndex4 = n);
+  showSlides4((slideIndex4 = n));
 }
 
 function showSlides4(n) {
   var i;
   var slides4 = document.getElementsByClassName("mySlides4");
   var dots4 = document.getElementsByClassName("dot4");
-  if (n > slides4.length) {slideIndex4 = 1}
-  if (n < 1) {slideIndex4 = slides4.length}
+  if (n > slides4.length) {
+    slideIndex4 = 1;
+  }
+  if (n < 1) {
+    slideIndex4 = slides4.length;
+  }
   for (i = 0; i < slides4.length; i++) {
-      slides4[i].style.display = "none";
+    slides4[i].style.display = "none";
   }
   for (i = 0; i < dots4.length; i++) {
-      dots4[i].className = dots4[i].className.replace(" active3", "");
+    dots4[i].className = dots4[i].className.replace(" active3", "");
   }
-  slides4[slideIndex4-1].style.display = "block";
-  dots4[slideIndex4-1].className += " active4";
+  slides4[slideIndex4 - 1].style.display = "block";
+  dots4[slideIndex4 - 1].className += " active4";
 }
-
 
 //for modal gallery on the index
 
- // Get the modal
- var modal = document.getElementById("myModal2");
+// Get the modal
+var modal = document.getElementById("myModal2");
 
- // Get the image and insert it inside the modal - use its "alt" text as a caption
- var img = document.getElementById("myImg");
- var modalImg = document.getElementById("img01");
- var captionText = document.getElementById("caption");
-function myModal2(source){
- //   modal.style.display = "block";
- //   modalImg.src = this.src;
- //   captionText.innerHTML = this.alt;
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+function myModal2(source) {
+  //   modal.style.display = "block";
+  //   modalImg.src = this.src;
+  //   captionText.innerHTML = this.alt;
 
- modal.style.display = "block"
- modalImg.src = source;
-
- }
-
- // Get the <span> element that closes the modal
- var span = document.getElementsByClassName("close2")[0];
-
- // When the user clicks on <span> (x), close the modal
- // span.onclick = function() {
- //   modal.style.display = "none";
- // }
-
-
-
-
-function myModal2Close (){
-  modal.style.display = "none";
+  modal.style.display = "block";
+  modalImg.src = source;
 }
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close2")[0];
 
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+function myModal2Close() {
+  modal.style.display = "none";
+}
